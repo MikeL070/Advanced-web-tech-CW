458a026ed5916e29a8456cead799939c275e0bd3
MikeL070 <07011787@live.napier.ac.uk>
2022-11-30 11:46:19 +0000

    py

--- main_v1.py
+++ main_v1.py
@@ -5,5 +5,9 @@ app = Flask(__name__, static_url_path = "", static_folder = "static")
 def title_page():
     return render_template('title_page.html')
 
+@app.route('/charChoice/')
+def charChoice():
+    return render_template('charClassChoice.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
